494b209c51da2826a3936890d7a149c2390ff6f5
Suolamainen <93546777+Suolamainen@users.noreply.github.com>
2022-04-28 10:17:07 +0300

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,20 @@
+#next line imports time module
+import time
+#next line has a = 10
+a = 10
+#next line prints countdown starts
+print("countdown starts")
+#next line
+while 1243250453463462905308236980925476 > a:
+    #next line prints valuable a which gets smaller everytime by the next line after the next line
+    print(a)
+    #next line lowers the valuable a after everytime it gets printed
+    a -= 1
+    #on the next line we use the module called "time". by typing 1 in the middle of the whatever its called () those, it waits 1 second before going to the next line. 
+    time.sleep(1)
+    #next line checks if a is 0 and if it is it chnages the value of the a to 1243250453463462905308236980925477
+    if a == 0:
+        a += 1243250453463462905308236980925477
+#next line prints hello world. It triggers after a is more than 1243250453463462905308236980925476.
+print("Helo wold!")
+#now you have printed hello world with style!
